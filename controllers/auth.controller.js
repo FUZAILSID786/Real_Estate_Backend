99e027d5c5e6ac8a1490e85228c6d32216758a49
Fuzail Ahmed Siddiqui <fuzailsiddiqui499@gmail.com>
2025-02-09 03:36:02 +0530

    8-2-25

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,31 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js"
 
 export const register = async (req, res) => {
     const {username, email, password} = req.body;
-    
-    // HASH THE PASSWORD
+
+    try{
+        // HASH THE PASSWORD
     const hashedPassword = await bcrypt.hash(password , 10);
     console.log(hashedPassword);
     // CREATE A NEW USER AND SAVE TO THE DATABASE
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password:hashedPassword,
+        },
+    })
+    console.log(newUser);
     
+    res.status(201).json({message:"User created successfully"})
+}catch(err){
+    console.log(err)
+    res.status(500).json({message:"Failed to create user!"})
+}
 }
 export const login = (req, res) => {
-    //db operations
+    const {username, password} = req.body;
 }
 export const logout = (req, res) => {
     //db operations

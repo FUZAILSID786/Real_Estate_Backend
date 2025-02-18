35d09935839a0d1af9146d41116ca8910619761b
Fuzail Ahmed Siddiqui <fuzailsiddiqui499@gmail.com>
2025-02-18 23:25:17 +0530

    18-2-25

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import prisma from "../lib/prisma.js"
 
 export const register = async (req, res) => {
@@ -24,6 +25,7 @@ export const register = async (req, res) => {
     res.status(500).json({message:"Failed to create user!"})
 }
 }
+
 export const login = async (req, res) => {
     const {username, password} = req.body;
 
@@ -44,13 +46,30 @@ export const login = async (req, res) => {
         if(!isPasswordValid) return res.status(401).json({message:"Invalid Credentials!"});
         
         //GENERATE AND SEND THE TOKEN TO THE USER
+        const age = 1000*60*60*24; 
 
+        const token = jwt.sign({
+            id:user.id
+        },
+        process.env.JWT_SECRET_KEY,
+        {expiresIn: age }
+    );
+
+    const {password:userPassword, ...userInfo} = user
+
+
+        res.cookie("token", token, {
+            httpOnly:true,
+            // secure:true,
+            maxAge:age,
+        }).status(200).json(userInfo);
         
     }catch(err){
         console.log(err)
         res.status(500).json({message:"Failed to login!"})
     }
 }
+
 export const logout = (req, res) => {
-    //db operations
+    res.clearCookie("token").status(200).json({ message: "Logout Successful" })
 }
